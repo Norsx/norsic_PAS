8e8724f94b98ecc9c307bb30d71daaf533966bd4
Norsx <ivan.norsic01@gmail.com>
2026-01-10 17:55:34 +0100

    ...

--- pas/zadatak2/build/zadatak2/prefix_override/sitecustomize.py
+++ pas/zadatak2/build/zadatak2/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ivan/Documents/PAS/zadatak2/projektiranje-autonomnih-sustava/zadatak2/install/zadatak2'
+    sys.prefix = sys.exec_prefix = '/home/ivan/Documents/PAS/zadatak2/pas/zadatak2/install/zadatak2'
